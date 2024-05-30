0a24cb5e1a7f600b08c5a41ec66b5468be3d7658
ljx <lk2236014477@163.com>
2024-05-30 13:11:07 +0800

    整体结构拆分和分类实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,4 +1,17 @@
+<script setup>
+import HomeCategory from '@/views/Home/components/HomeCategory.vue'
+import HomeBanner from '@/views/Home/components/HomeBanner.vue'
+import HomeNew from '@/views/Home/components/HomeNew.vue'
+import HomeHot from '@/views/Home/components/HomeHot.vue'
+import homeProduct from '@/views/Home/components/HomeProduct.vue'
+</script>
+
 <template>
-  <div>我是Home</div>
-  <div style="height: 1200px;"></div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
