1861db5098e1161b32ca851f36e4169fa6b55ea7
ljx <lk2236014477@163.com>
2024-05-31 13:22:07 +0800

    解决路由缓存

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,16 +4,24 @@ import { getTopCategoryAPI } from '@/apis/Category.js'
 import { useRoute } from 'vue-router'
 import {  ref ,onMounted} from 'vue'
 import GoodsItem from '../Home/components/GoodsItem.vue'
+import { onBeforeRouteUpdate } from 'vue-router'
   const categoryData = ref({})
   const route = useRoute()
-  const getCategory = async (id) => {
+  const getCategory = async (id = route.params.id) => {
     // 如何在setup中获取路由参数 useRoute() -> route 等价于this.$route
-
     const res = await getTopCategoryAPI(id)
     console.log(res)
     categoryData.value = res.result
   }
-  onMounted(() => getCategory(route.params.id))
+  onMounted(() => getCategory())
+
+  // 目标:路由参数变化的时候 可以把分类数据接口重新发送
+  onBeforeRouteUpdate((to) => {
+    // 存在问题：使用最新的路由参数请求最新的分类数据
+    getCategory(to.params.id)
+  })
+ 
+
 
 //获取banner
  
