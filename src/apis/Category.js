2d8357284669cbe4ff7ba27df9bf4eaed65c3b2b
ljx <lk2236014477@163.com>
2024-05-31 15:05:08 +0800

    二级导航面包屑导航

--- src/apis/Category.js
+++ src/apis/Category.js
@@ -1,7 +1,20 @@
 import httpInstance from '@/utils/http.js'
+
+//获取一级分类数据
 export const getTopCategoryAPI = (id) => {
   return httpInstance({
     url:'/category',
+    params:{
+      id
+    }
+  })
+}
+
+//获取二级分类数据
+
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url:'/category/sub/filter',
     params:{
       id
     }
