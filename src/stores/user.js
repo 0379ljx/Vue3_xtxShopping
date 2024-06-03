d5dbe09aa35a157944ce7566a252783ca91ce796
ljx <lk2236014477@163.com>
2024-06-03 17:13:10 +0800

    合并购物车

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,6 +2,8 @@ import { defineStore} from 'pinia'
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/User.js'
 import { useCartStore } from './cartStore.js'
+import { mergeCartAPI} from '@/apis/Cart.js'
+
 export const useUserStore = defineStore('user', () => {
   const cartStore =useCartStore()
   // 1. 定义管理用户数据的state
@@ -10,6 +12,15 @@ export const useUserStore = defineStore('user', () => {
   const getUserInfo = async ({ account, password }) => {
     const res = await loginAPI({ account, password })
     userInfo.value = res.result
+    //合并购物车操作
+    await mergeCartAPI(cartStore.cartList.map(item =>{
+      return {
+        skuId:item.skuId,
+        selected:item.selected,
+        count:item.count
+      }
+    }))
+    cartStore.updateNewList()
   }
    // 退出时清除用户信息
    const clearUserInfo = () => {
