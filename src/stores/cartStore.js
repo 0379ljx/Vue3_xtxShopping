560b7cbaaf81f47d78e5a284831f26df081896ef
ljx <lk2236014477@163.com>
2024-06-03 15:16:21 +0800

    本地购物车列表购物车全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -29,6 +29,13 @@ export const useCartStore = defineStore('cart', () => {
       const idx = cartList.value.findIndex((item) => skuId === item.skuId)
       cartList.value.splice(idx, 1)
   }
+
+ // 单选功能
+const singleCheck = (skuId, selected) => {
+  // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+  const item = cartList.value.find((item) => item.skuId === skuId)
+  item.selected = selected
+}
    // 计算属性
   // 1. 总的数量 所有项的count之和
   const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
@@ -50,7 +57,8 @@ export const useCartStore = defineStore('cart', () => {
     allPrice,
     selectedCount,
     selectedPrice,
-    isAll
+    isAll,
+    singleCheck
     
   }
 }, {
