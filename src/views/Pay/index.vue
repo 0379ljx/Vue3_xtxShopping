875a3fe39dfb7dd0b9bca6b1ef27fad45f543ac3
ljx <lk2236014477@163.com>
2024-06-04 14:17:12 +0800

    实现支付功能

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -11,8 +11,12 @@ const getPayInfo = async () => {
 }
 onMounted(() => getPayInfo())
 
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
-
 
 <template>
   <div class="xtx-pay-page">
@@ -22,7 +26,7 @@ onMounted(() => getPayInfo())
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formatTime }}</span>, 超时后将取消订单</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
